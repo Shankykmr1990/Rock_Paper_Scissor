eca5a1a22596b6185119f32476232ca61c6d5a17
shash <shash@SHASHANK>
2023-12-08 20:29:15 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock_Paper_Scissor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool playAgain = true;
+            String player;
+            String computer;
+            
+
+            while (playAgain)
+            {
+                player = "";
+                computer = "";
+
+                while (player != "ROCK" && player != "PAPER" && player != "SCISSORS")
+                {
+                    Console.WriteLine("Select From ROCK, PAPER and SCISSORS");
+                    player = Console.ReadLine();
+                    player = player.ToUpper();
+                    
+                    
+                }
+                //Console.WriteLine("You Select: " + player);
+                int randomNum = random.Next(1, 4);
+                switch (randomNum)
+                {
+                    case 1:
+                        computer = "ROCK";
+                        break;
+                    case 2:
+                        computer = "PAPER";
+                        break;
+                    case 3:
+                        computer = "SCISSORS";
+                        break;
+                }
+                Console.WriteLine("Player: " + player);
+                Console.WriteLine("Computer: " + computer);
+                
+                switch(player)
+                {
+                    case "ROCK":
+                        if (computer == "ROCK")
+                        {
+                            Console.WriteLine("It's a Draw!");
+                        }
+                        else if (computer == "PAPER")
+                        {
+                            Console.WriteLine("You Lose!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You Win!");
+                        }
+                        break;
+                    case "PAPER":
+                        if (computer == "PAPER")
+                        {
+                            Console.WriteLine("It's a Draw!");
+                        }
+                        else if (computer == "ROCK")
+                        {
+                            Console.WriteLine("You Win!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You Lose!");
+                        }
+                        break;
+                    case "SCISSORS":
+                        if (computer == "SCISSOR")
+                        {
+                            Console.WriteLine("It's a Draw!");
+                        }
+                        else if (computer == "PAPER")
+                        {
+                            Console.WriteLine("You Lose!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You Win!");
+                        }
+                        break;
+                        
+                }
+                
+            }
+            Console.WriteLine("Thanks for Playing...");
+            Console.ReadLine();
+        }
+    }
+}
